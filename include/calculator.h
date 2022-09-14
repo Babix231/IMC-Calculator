5b0eb11e449f463b1d514a5c737f5472ac818471
Ouadah <sabri.ouadah@epitech.eu>
2022-09-14 09:26:33 +0200

    09:26 [+]: adding Include with struct

---
+++ include/calculator.h
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2022
+** IMC-Calculator
+** File description:
+** Calculator
+*/
+
+#pragma once
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+
+typedef struct {
+    float weight;
+    float height;
+    float result;
+} struct_t;
